4ef931052eaf4e535fd96df629ceee72b1aa1ef0
Vingonc <vinicius.gcosta@sptech.school>
2025-07-28 18:26:42 -0300

    Função para deletar imagem + estilização no carregamento da foto

--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./FeedPhotosItem.module.css";
+import Image from "../Helper/Image";
 
 const FeedPhotosItem = ({ photo }) => {
   function handleClick() {
@@ -8,7 +9,7 @@ const FeedPhotosItem = ({ photo }) => {
 
   return (
     <li className={styles.photo} onClick={handleClick}>
-      <img src={photo.src} alt={photo.title} />
+      <Image src={photo.src} alt={photo.title} />
       <span className={styles.visualizacao}>{photo.acessos}</span>
     </li>
   );
